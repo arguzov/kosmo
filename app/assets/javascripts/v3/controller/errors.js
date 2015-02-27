5e967599b48e51265a84c177c7c9f91edf82a1df
axxil <axxil@yandex.ru>
2015-02-27 17:18:21 +0300

    minor fixes

---
+++ app/assets/javascripts/v3/controller/errors.js
@@ -0,0 +1,6 @@
+//=require v3/common
+//=require v3/plugins/owl-carousel/owl.carousel.min
+//=require v3/plugins/magnific-popup/jquery.magnific-popup.min
+//=require v3/plugins/jquery.appear
+//=require v3/plugins/jquery.countTo
+//=require v3/plugins/jquery.parallax-1.1.3
