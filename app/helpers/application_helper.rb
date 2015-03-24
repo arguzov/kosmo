365ef442e8019ec8ffb5d2b56857ac75b596cf93
axxil <axxil@yandex.ru>
2015-03-24 13:47:35 +0300

    routine fix

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -16,4 +16,32 @@ module ApplicationHelper
     def current_layout
         return params[:controller].split("/").first
     end
+
+    def month(num)
+        if num == '01'
+            return 'января'
+        elsif num == '02'
+            return 'февраля'
+        elsif num == '03'
+            return 'марта'
+        elsif num == '04'
+            return 'апреля'
+        elsif num == '05'
+            return 'мая'
+        elsif num == '06'
+            return 'июня'
+        elsif num == '07'
+            return 'июля'
+        elsif num == '08'
+            return 'августа'
+        elsif num == '09'
+            return 'сентября'
+        elsif num == '10'
+            return 'октября'
+        elsif num == '11'
+            return 'ноября'
+        elsif num == '12'
+            return 'декабря'
+        end
+    end
 end
