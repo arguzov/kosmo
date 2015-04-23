116724c256e3c2bfef6ad491116e1da46346fecc
axxil <axxil@yandex.ru>
2015-04-23 16:07:03 +0300

    fix register visit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  before_action :register_visit, only: ['show']
+  #before_action :register_visit, only: ['show']
 
   def authenticate_admin_user!
       authenticate_user!
