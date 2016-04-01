9ebbe14b77a8f45defb8187bb81669ccd4d9c01a
axxil <axxil@yandex.ru>
2016-04-01 15:06:42 +0300

    fix medicaments

--- app/controllers/medicaments_controller.rb
+++ app/controllers/medicaments_controller.rb
@@ -4,5 +4,6 @@ class MedicamentsController < ApplicationController
         @medicament = Medicament.find(params[:id])
         @same = Medicament.where('service_id = ?',@medicament.service_id)
         @pricelist = @medicament.service.pricelist.split(/\n/)
+        @service = @medicament.service
     end
 end
