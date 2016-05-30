13db2c84035f1d0c6ec7464c99cdb99f4ea545d3
axxil <axxil@yandex.ru>
2016-05-30 10:59:13 +0300

    fix prices

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -2,7 +2,7 @@ class SectionsController < ApplicationController
 
     def show
         @section = Section.where({:url=>params[:url]}).first
-        @prices = Price.select('(price - (MIN(price)*discount)/100) as min_price,price,service_id').where('parent_id > 0').group('service_id').index_by(&:service_id)
+        @prices = Price.select('MIN(price - ((price*discount)/100)) AS min_price,discount,price,service_id').where('price > 0').group('service_id').index_by(&:service_id)
     end
 
 end
