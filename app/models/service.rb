0622f06503c0656d853119e2a066e8f8cdd804a5
axxil <axxil@yandex.ru>
2017-12-20 08:56:02 +0300

    added faq block to service page

--- app/models/service.rb
+++ app/models/service.rb
@@ -2,6 +2,7 @@ class Service < ActiveRecord::Base
     has_attached_file :photo, :styles => { :medium => "300x300", :thumb => "100x100" }, :default_url => "/images/:style/missing.png"
     validates_attachment_content_type :photo, :content_type => /\Aimage\/.*\Z/
     has_many :medicaments
+    has_many :faqs
     has_and_belongs_to_many :sections
     has_and_belongs_to_many :issues
     has_and_belongs_to_many :posts
