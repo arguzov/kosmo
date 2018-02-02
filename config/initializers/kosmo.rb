86ff8a1f166e0b1745941cea902afc16a926cce7
axxil <axxil@yandex.ru>
2018-02-02 13:13:19 +0300

    fix lazer promo

--- config/initializers/kosmo.rb
+++ config/initializers/kosmo.rb
@@ -1,4 +1,4 @@
-MANAGER_EMAIL = 'epi.center.kazan@yandex.ru'
+MANAGER_EMAIL = 'epi.center.kazan@gmail.com'
 #MANAGER_EMAIL = 'axxil@yandex.ru'
 FROM_EMAIL = 'feedback@kosmetologiya-kazan.ru'
 NO_TOP_BANNER_PAGES = ['promos']
